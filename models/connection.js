4607c52bcfcbe76c103ed8eaffd671958baf4e88
Marion Getty-Tehan <mgettytehan@gmail.com>
2019-08-21 19:02:19 -0400

    Added user model basic functions and successfully tested. Also added mongoose option in response to deprecation warning.

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,9 @@
-/* Import the mongoose module
- *
- */
+// Import the mongoose module
 const mongoose = require('mongoose');
+
+// avoid deprecation warning on some find methods
+// see https://mongoosejs.com/docs/deprecations.html#-findandmodify
+mongoose.set('useFindAndModify', false);
 
 const connectionString = process.env.MONGODB_URI || "mongodb://localhost/fandomlist";
 
--- models/user.js
+++ models/user.js
@@ -1,15 +1,38 @@
-const mongoose = require('./connection.js')
+const mongoose = require('./connection.js');
 
 const UserSchema = new mongoose.Schema({
   username: String,
   bio: String,
   link: String
-})
+});
 
-const UserCollection = mongoose.model('User', UserSchema)
+const UserCollection = mongoose.model('User', UserSchema);
 
+function getAllUsers() {
+  return UserCollection.find();
+}
+
+function getUser(userId) {
+  return UserCollection.findById(userId);
+}
+
+function addUser(newUser) {
+  return UserCollection.insertMany(newUser);
+}
+
+function updateUser(userId, changedUser) {
+  return UserCollection.findByIdAndUpdate(userId, changedUser);
+}
+
+function deleteUser(userId) {
+  return UserCollection.findByIdAndDelete(userId);
+}
 
 
 module.exports = {
-
+  addUser,
+  deleteUser,
+  getAllUsers,
+  getUser,
+  updateUser
 }
