b0d2e72f7b3f132e66e040b63d4d175b7b186c0e
Marion Getty-Tehan <mgettytehan@gmail.com>
2019-08-21 19:26:39 -0400

    Added mediatype model with basic functions

---
+++ models/mediatype.js
@@ -0,0 +1,36 @@
+const mongoose = require('./connection.js');
+
+const MediaTypeSchema = new mongoose.Schema({
+  typename: String
+});
+
+const MediaTypeCollection = mongoose.model('MediaType', MediaTypeSchema);
+
+function getAllMediaTypes() {
+  return MediaTypeCollection.find();
+}
+
+function getMediaType(typeId) {
+  return MediaTypeCollection.findById(typeId);
+}
+
+function addMediaType(mediaType) {
+  return MediaTypeCollection.insertMany(mediaType);
+}
+
+function updateMediaType(typeId, changedMediaType) {
+  return MediaTypeCollection.findByIdAndUpdate(typeId, changedMediaType);
+}
+
+function deleteMediaType(typeId) {
+  return MediaTypeCollection.findByIdAndDelete(typeId);
+}
+
+
+module.exports = {
+  addMediaType,
+  deleteMediaType,
+  getAllMediaTypes,
+  getMediaType,
+  updateMediaType
+}
