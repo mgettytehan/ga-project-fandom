bb9f08bf6267074776b3e16876f5d1d603d7561a
Marion Getty-Tehan <mgettytehan@gmail.com>
2019-08-21 21:53:23 -0400

    Added get and post for /users route

--- controllers/user.js
+++ controllers/user.js
@@ -4,6 +4,21 @@ const userApi = require('../models/user.js');
 
 const userRouter = express.Router();
 
+userRouter.route('/')
+  .get( (req, res) => {
+    userApi.getAllUsers().then(
+      users => res.send(users)
+    ).catch(
+      () => res.sendStatus(400)
+    );
+  })
+  .post( (req, res) => {
+    userApi.addUser(req.body).then(
+      () => res.sendStatus(201)
+    ).catch(
+      () => res.sendStatus(400)
+    );
+  });
 
 module.exports = {
   userRouter
