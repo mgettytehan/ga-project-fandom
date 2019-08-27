a3f76ffa436625fed3a0f7fa7e762beb5c99b251
Marion Getty-Tehan <mgettytehan@gmail.com>
2019-08-27 10:10:29 -0400

    Fixed select helper function and fandom edit page to select existing value correctly

--- hbshelpers/hhelpers.js
+++ hbshelpers/hhelpers.js
@@ -6,10 +6,9 @@ hbs.registerHelper('selectedValue',
     (valueOne, valueTwo) => {
         if (valueOne === undefined || valueTwo === undefined)
             return '';
-        if (valueOne == valueTwo)
-            return 'selected="selected"';
-        else
-            return '';
+        if (String(valueOne) == String(valueTwo))
+            return 'selected = "selected"';
+        return '';
     }
 );
 
